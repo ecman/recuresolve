ca8f1fa727f11dd03691afbf162bb8ba17c6eb6e
ecman <ecman@users.noreply.github.com>
2017-05-15 15:26:06 -0700

    Add test

---
+++ test/index.js
@@ -0,0 +1,48 @@
+'use strict';
+const recuresolve = require('../');
+const assert = require('assert');
+const DELAY = 0.5;
+
+function asyncAdder(num1, num2, callback) {
+  if (typeof(num1) !== 'number' ||
+      typeof(num2) !== 'number') {
+    let err = new TypeError('Parameters must be numbers');
+    setTimeout(() => callback(err, 0), 1000 * DELAY);
+    return;
+  }
+
+  let total = num1 + num2;
+  setTimeout(() => callback(null, total), 1000 * DELAY);
+}
+
+const recursiveAsyncAdderCaller = recuresolve(
+  (thisFn, resolve, reject, nums, index = 0, total = 0) => {
+
+  if (index === nums.length) {
+    console.log(`resolving total: ${total}`);
+    resolve(total);
+  } else {
+    let num = nums[index];
+    console.log(`Adding ${num} to total ${total}`);
+    asyncAdder(num, total, (err, newTotal) => {
+      if (err) {
+        reject(err);
+      } 
+      else {
+        thisFn(thisFn, resolve, reject, nums, index + 1, newTotal);
+      }
+    });
+  }
+
+});
+
+recursiveAsyncAdderCaller([1, 2, 3, 4, 5])
+  .then((sumTotal) => assert
+    .strictEqual(sumTotal, 15, 
+      `sumTotal should be 15 not ${sumTotal}`));
+      
+recursiveAsyncAdderCaller([1, 2, 3, 4, '5'])
+  .then((sumTotal) => assert
+    .fail(sumTotal, 0, 'Rejections should not resolve'))
+  .catch((err) => assert
+    .ok());
